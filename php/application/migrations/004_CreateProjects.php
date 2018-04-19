556fa6031932175551d2ab444c2447e6039f9324
jsbry <jsbry.biz@gmail.com>
2018-04-19 23:59:36 +0900

    update: プロジェクト用マイグレーションファイル更新

--- php/application/migrations/004_CreateProjects.php
+++ php/application/migrations/004_CreateProjects.php
@@ -1,54 +1,49 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Migration_CreateAccessTokens extends CI_Migration
+class Migration_CreateProjects extends CI_Migration
 {
 	/**
 	 * up
 	 *
-	 * access_tokensテーブル作成
+	 * projectsテーブル作成
 	 */
 	public function up()
 	{
 		$fields = array(
-			'access_token' => array(
-				'type'           => 'char',
-				'constraint'     => '128',
-				'unique'         => TRUE,
-				'null'           => FALSE,
-				'comment'        => 'アクセストークン'
-			),
-			'user_id' => array(
+			'project_id' => array(
 				'type'           => 'bigint',
 				'unsigned'       => TRUE,
-				'comment'        => '利用者ID'
+				'auto_increment' => TRUE,
+				'comment'        => 'プロジェクトID'
 			),
-			'ip_address' => array(
+			'name' => array(
 				'type'           => 'varchar',
-				'constraint'     => '15',
-				'comment'        => 'IPアドレス'
-			),
-			'access_expire' => array(
-				'type'           => 'datetime',
+				'constraint'     => '255',
 				'null'           => FALSE,
-				'comment'        => 'ログイン期限'
+				'comment'        => '名称'
+			),
+			'description' => array(
+				'type'           => 'text',
+				'null'           => FALSE,
+				'comment'        => '説明'
 			),
 		);
 		$this->dbforge->add_field($fields);
 		$this->dbforge->add_field('created datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT "登録日時"');
 		$this->dbforge->add_field('modified datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "更新日時"');
 
-		$this->dbforge->add_key('access_token', TRUE);
-		$this->dbforge->create_table('access_tokens');
+		$this->dbforge->add_key('projects', TRUE);
+		$this->dbforge->create_table('projects');
 	}
 
 	/**
 	 * down
 	 *
-	 * access_tokensテーブル削除
+	 * projectsテーブル削除
 	 */
 	public function down()
 	{
-		$this->dbforge->drop_table('access_tokens', TRUE);
+		$this->dbforge->drop_table('projects', TRUE);
 	}
 }
