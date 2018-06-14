a283b8a91c6e1032f36914761f111eddc683e72c
jsbry <jsbry.biz@gmail.com>
2018-06-15 01:04:08 +0900

    add: カテゴリテスト追加

---
+++ php/application/tests/models/Categories_test.php
@@ -0,0 +1,53 @@
+<?php
+
+class Categories_test extends TestCase
+{
+	public function setUp()
+	{
+		$this->resetInstance();
+
+		$this->CI->load->library('migration');
+		$this->CI->migration->version(0);
+		$this->CI->migration->latest();
+
+		$this->CI->load->model('Categories_model', 'Categories');
+	}
+
+	/**
+	 * @test
+	 */
+	public function search()
+	{
+		$result = $this->CI->Projects->search();
+		$this->assertEmpty($result);
+	}
+
+	/**
+	 * @test
+	*/
+	public function get()
+	{
+		$result = $this->CI->Projects->get();
+		$this->assertNull($result);
+	}
+
+	/**
+	 * @test
+	*/
+	public function update()
+	{
+		$data = array();
+		$result = $this->CI->Projects->update(0, $data);
+		$this->assertFalse($result);
+	}
+
+	/**
+	 * @test
+	*/
+	public function insert()
+	{
+		$data = array('name' => 'name');
+		$result = $this->CI->Projects->insert($data);
+		$this->assertEquals('name', $result->name);
+	}
+}
